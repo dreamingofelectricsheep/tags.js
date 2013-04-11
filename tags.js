02e98308c9a75919b8c922c7b1173e2fa66c0ffa
Bob <bob@bob.bob>
2013-04-11 17:40:52 +0200

    Initial commit, code that has been going in one form or another in various project of mine.

---
+++ tags.js
@@ -0,0 +1,79 @@
+(function() {
+
+each = function(obj, f)
+{
+	if(obj instanceof Array)
+		for(var i = 0; i < obj.length; i++)
+			f(obj[i], i, obj)
+	else
+		for(var i in obj)
+			f(obj[i], i, obj)
+}
+
+range = function()
+{
+	var r = [],
+		step = 1,
+		start = 0,
+		end = 0
+
+	switch(arguments.length)
+	{
+		case 1:
+			end = arguments[0]
+			break
+		case 2:
+			start = arguments[0]
+			end = arguments[1]
+			break
+		case 3:
+			start = arguments[0]
+			end = arguments[1]
+			step = arguments[2]
+			break
+	}
+	
+	for(var i = start; i < end; i += step)
+		r.push(i)
+
+	return r
+}
+
+})()
+
+
+function tags(tag, options, children) {
+	var element = document.createElement(tag)
+
+	for(var i in options)
+		element.setAttribute(i, options[i])
+
+
+	for(var i in children)
+		if(typeof children[i] == 'object')
+		{
+			element.appendChild(children[i])
+
+			if(children[i].id != undefined)
+				element['$' + children[i].id] = children[i]
+		}
+		else
+			element.innerHTML = children[i]
+
+	return element
+}
+
+(function() {
+	for(var i in arguments)
+		(function(tag) { 
+			tags[tag] = function(options) { 
+				var children = Array.prototype.slice.call(arguments)
+
+				if(typeof options != undefined)
+					children = children.slice(1)
+
+				return tags(tag, options, children) 
+			} 
+		})(arguments[i])
+})('html', 'div', 'p', 'input', 'body', 'a', 'textarea', 'canvas',
+	'td', 'tr', 'table')
